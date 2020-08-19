43f1cfe09e627f0e237de6cfc621f22fa448f592
ehtishamali042@gmail.com <ehtishamali042@gmail.com>
2020-08-19 02:16:48 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import { SEO, MainLayout } from "../components"
+
+const IndexPage = () => (
+  <MainLayout>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+  </MainLayout>
+)
+
+export default IndexPage
